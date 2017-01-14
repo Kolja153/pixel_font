adf279294f8536f611f5debd381f8f44d584d431
Mykola Fedan <kolja153@gmail.com>
2017-01-14 11:26:34 +0200

    add input row & column

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-createTable ();
-var mass = createMass();
+var mass ;
+var row ;
+var column ;
+
+newTable();
 
 $( "td" ).click(function() {
-	var color = this;
-	
-	colColor(color);
-
-	
+	console.log('this');
+	colColor(this);	
 
 	if (mass[this.id[6]][this.id[7]] == 0){
 		mass[this.id[6]][this.id[7]] = 1;
@@ -16,32 +16,42 @@ $( "td" ).click(function() {
 		mass[this.id[6]][this.id[7]] = 0;
 	}
 	
-	createLine(mass);	
+	createLine(mass,row);	
 })
 
-function createLine(mass) {
+function newTable(){
+	$("#content").text('');
+
+	row = $("#row").val();
+	column = $("#column").val();	
+	mass = createMass(row,column);
+
+	createTable (row,column);
+}
+
+function createLine(mass,rmax) {
 	$( "#array").text('');
 	var string='';
-	for(var i = 0; i < 7; i++) {
+	for(var i = 0; i < rmax; i++) {
 		var line = mass[i];
 		line=line.join();
 		console.log( );
 		
-		$( "#line"+i).text( "line"+i+'     bin = '+line+'      hex = 0x' +ConvertBase.bin2hex(line.replace(/,/g, '')));
+		$( "#line"+i).text( "row    "+i+'     bin = '+line+'      hex = 0x' +ConvertBase.bin2hex(line.replace(/,/g, '')));
 			string=string+'0x'+ConvertBase.bin2hex(line.replace(/,/g,''));
-			if (i<6){string=string+','};
+			if (i<rmax-1){string=string+','};
 			$( "#array").text(string);
 
 		//$( "#array").append( '0x'+ConvertBase.bin2hex(line.replace(/,/g, ''))+',');		
 	}
 }
 
-function createMass() {
-
+function createMass(rmax,cmax) {
+	var rmax,cmax;
 	var row = [];
-	for(var i = 0; i < 7; i++) {
+	for(var i = 0; i < rmax; i++) {
 		var column = [];
-		for(var j = 0; j < 8; j++) {
+		for(var j = 0; j < cmax; j++) {
 			column[j] = 0;
 		}
 		row[i] = column ;
@@ -73,21 +83,21 @@ function colColor(color){
 	}
 }
 
-function createTable (){ 
+function createTable (rmax,cmax){ 
 	
 	$( "#content").append( '<table id="symbol" height="160" width="180" border="1"></table>	');
-	for(var i = 0; i < 7; i++) {
-		$( "#content").append( '<br><label id ="line'+i+'">line'+i+' = 0,0,0,0,0,0,0,0 </label>');
+	for(var i = 0; i < rmax; i++) {
+		$( "#content").append( '<br><label id ="line'+i+'">row '+i+' = </label>');
 		}
-	for(var i = 0; i < 7; i++) {
+	for(var i = 0; i < rmax; i++) {
   		 $( "#symbol" ).append( '<tr id ="row'+i+'""></tr>' );
 
-	   for (var j = 0; j < 8; j++) {
+	   for (var j = 0; j < cmax; j++) {
 		$( "#row"+i ).append( '<td id ="column'+i+j+'" class="noclick"> </td>' );
 
 	   }  
 	}
 
-$( "#content").append( '<p>array = {<span id ="array">0x0,0x0,0x0,0x0,0x0,0x0,0x0</span>}</p>');
+$( "#content").append( '<p>array = {<span id ="array"></span>}</p>');
 $( "#content").append( '<p></p><button onclick="cleartable()">очистити</button>' );
 }
